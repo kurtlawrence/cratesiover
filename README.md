1a8392996a9d74226835b4a057fe8bba8feebbaf
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-02-27 19:19:40 +1000

    added current version output into result

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-[![Build Status](https://travis-ci.com/kurtlawrence/cratesiover.svg?branch=master)](https://travis-ci.com/kurtlawrence/cratesiover) [![Latest Version](https://img.shields.io/crates/v/cratesiover.svg)](https://crates.io/crates/cratesiover) [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cratesiover) [![codecov](https://codecov.io/gh/kurtlawrence/cratesiover/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cratesiover)
-[![Rustc Version 1.30+](https://img.shields.io/badge/rustc-1.30+-blue.svg)](https://blog.rust-lang.org/2018/10/25/Rust-1.30.0.html)
+[![Build Status](https://travis-ci.com/kurtlawrence/cratesiover.svg?branch=master)](https://travis-ci.com/kurtlawrence/cratesiover)
+[![Latest Version](https://img.shields.io/crates/v/cratesiover.svg)](https://crates.io/crates/cratesiover)
+[![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cratesiover)
+[![codecov](https://codecov.io/gh/kurtlawrence/cratesiover/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cratesiover)
 
 Query and compare the semver of a crate on crates.io.
 
@@ -12,8 +14,8 @@ use cratesiover::Status;
 let query = cratesiover::query("cratesiover", &env!("CARGO_PKG_VERSION")).unwrap();
 
 match query {
-  Status::Behind => println!("crate is behind the version on crates.io"),
-  Status::Equal => println!("crate is equal to the version on crates.io"),
-  Status::Ahead => println!("crate is ahead of the version on crates.io"),
+  Status::Behind(ver) => println!("crate is behind the version on crates.io {}", ver),
+  Status::Equal(ver) => println!("crate is equal to the version on crates.io {}", ver),
+  Status::Ahead(ver) => println!("crate is ahead of the version on crates.io {}", ver),
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
-//! [![Build Status](https://travis-ci.com/kurtlawrence/cratesiover.svg?branch=master)](https://travis-ci.com/kurtlawrence/cratesiover) [![Latest Version](https://img.shields.io/crates/v/cratesiover.svg)](https://crates.io/crates/cratesiover) [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cratesiover) [![codecov](https://codecov.io/gh/kurtlawrence/cratesiover/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cratesiover)
-//! [![Rustc Version 1.30+](https://img.shields.io/badge/rustc-1.30+-blue.svg)](https://blog.rust-lang.org/2018/10/25/Rust-1.30.0.html)
-//!
+//! [![Build Status](https://travis-ci.com/kurtlawrence/cratesiover.svg?branch=master)](https://travis-ci.com/kurtlawrence/cratesiover)
+//! [![Latest Version](https://img.shields.io/crates/v/cratesiover.svg)](https://crates.io/crates/cratesiover)
+//! [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cratesiover)
+//! [![codecov](https://codecov.io/gh/kurtlawrence/cratesiover/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cratesiover)
+//! 
 //! Query and compare the semver of a crate on crates.io.
-//!
+//! 
 //! See the [rs docs](https://docs.rs/cratesiover/). [Github repo.](https://github.com/kurtlawrence/cratesiover)
-//!
+//! 
 //! # Example
-//!
+//! 
 //! ```rust
 //! use cratesiover::Status;
 //! let query = cratesiover::query("cratesiover", &env!("CARGO_PKG_VERSION")).unwrap();
-//!
+//! 
 //! match query {
-//!   Status::Behind => println!("crate is behind the version on crates.io"),
-//!   Status::Equal => println!("crate is equal to the version on crates.io"),
-//!   Status::Ahead => println!("crate is ahead of the version on crates.io"),
+//!   Status::Behind(ver) => println!("crate is behind the version on crates.io {}", ver),
+//!   Status::Equal(ver) => println!("crate is equal to the version on crates.io {}", ver),
+//!   Status::Ahead(ver) => println!("crate is ahead of the version on crates.io {}", ver),
 //! }
 //! ```
 #![warn(missing_docs)]
@@ -24,14 +26,15 @@ use semver::Version;
 use std::cmp::Ordering;
 
 /// The comparitive status of the version query.
+/// Each variant contains the `crates.io` version number.
 #[derive(Debug, PartialEq)]
 pub enum Status {
 	/// The version is behind the one on `crates.io`.
-	Behind,
+	Behind(Version),
 	/// The version is equal to the one on `crates.io`.
-	Equal,
+	Equal(Version),
 	/// The version is ahead of the one on `crates.io`.
-	Ahead,
+	Ahead(Version),
 }
 
 /// Errors in requesting or parsing the query.
@@ -56,11 +59,15 @@ pub fn get(crate_name: &str) -> Result<Version, Error> {
 /// ```rust
 /// use cratesiover::{ query, Status };
 /// let query = query("cratesiover", "0.1.0").unwrap();
-/// assert_eq!(query, Status::Behind);
+/// match query {
+///  Status::Behind(ver) => println!("crate is behind the version on crates.io {}", ver),
+///  Status::Equal(ver) => println!("crate is equal to the version on crates.io {}", ver),
+///  Status::Ahead(ver) => println!("crate is ahead of the version on crates.io {}", ver),
+/// }
 /// ```
 pub fn query(crate_name: &str, version: &str) -> Result<Status, Error> {
 	let version = Version::parse(version).map_err(|e| Error::SemVerError(e))?;
-	Ok(cmp(&version, &get(crate_name)?))
+	Ok(cmp(&version, get(crate_name)?))
 }
 
 fn parse(text: &str) -> Result<&str, Error> {
@@ -79,11 +86,11 @@ fn web_req(crate_name: &str) -> Result<String, Error> {
 		.map_err(|e| Error::RequestError(e))
 }
 
-fn cmp(current: &Version, cratesio: &Version) -> Status {
-	match current.cmp(cratesio) {
-		Ordering::Less => Status::Behind,
-		Ordering::Equal => Status::Equal,
-		Ordering::Greater => Status::Ahead,
+fn cmp(current: &Version, cratesio: Version) -> Status {
+	match current.cmp(&cratesio) {
+		Ordering::Less => Status::Behind(cratesio),
+		Ordering::Equal => Status::Equal(cratesio),
+		Ordering::Greater => Status::Ahead(cratesio),
 	}
 }
 
@@ -107,25 +114,18 @@ fn test_web_req() {
 
 #[test]
 fn cmp_test() {
+	let one_pt_oh = Version::parse("1.0.0").unwrap();
+	let pt_one_oh = Version::parse("0.1.0").unwrap();
 	assert_eq!(
-		cmp(
-			&Version::parse("1.0.0").unwrap(),
-			&Version::parse("1.0.0").unwrap()
-		),
-		Status::Equal
+		cmp(&one_pt_oh, one_pt_oh.clone(),),
+		Status::Equal(one_pt_oh.clone())
 	);
 	assert_eq!(
-		cmp(
-			&Version::parse("0.1.0").unwrap(),
-			&Version::parse("1.0.0").unwrap()
-		),
-		Status::Behind
+		cmp(&pt_one_oh, one_pt_oh.clone(),),
+		Status::Behind(one_pt_oh.clone())
 	);
 	assert_eq!(
-		cmp(
-			&Version::parse("1.0.0").unwrap(),
-			&Version::parse("0.0.1").unwrap()
-		),
-		Status::Ahead
+		cmp(&one_pt_oh, pt_one_oh.clone()),
+		Status::Ahead(pt_one_oh.clone())
 	);
 }
