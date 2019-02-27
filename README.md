b678375f74801fdc0298b7ae0c6e2966b568c122
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-02-27 18:33:53 +1000

    ready for publish, check ci

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-[![Build Status](https://travis-ci.com/kurtlawrence/papyrus.svg?branch=master)](https://travis-ci.com/kurtlawrence/papyrus) [![Latest Version](https://img.shields.io/crates/v/papyrus.svg)](https://crates.io/crates/papyrus) [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/papyrus) [![codecov](https://codecov.io/gh/kurtlawrence/papyrus/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/papyrus)
+[![Build Status](https://travis-ci.com/kurtlawrence/cratesiover.svg?branch=master)](https://travis-ci.com/kurtlawrence/cratesiover) [![Latest Version](https://img.shields.io/crates/v/cratesiover.svg)](https://crates.io/crates/cratesiover) [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cratesiover) [![codecov](https://codecov.io/gh/kurtlawrence/cratesiover/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cratesiover)
 [![Rustc Version 1.30+](https://img.shields.io/badge/rustc-1.30+-blue.svg)](https://blog.rust-lang.org/2018/10/25/Rust-1.30.0.html)
 
-A rust REPL and script running tool.
+Query and compare the semver of a crate on crates.io.
 
-See the [rs docs](https://docs.rs/papyrus/).
-Look at progress and contribute on [github.](https://github.com/kurtlawrence/papyrus)
+See the [rs docs](https://docs.rs/cratesiover/). [Github repo.](https://github.com/kurtlawrence/cratesiover)
 
-```sh
-papyrus=> 2+2
-papyrus [out0]: 4
+# Example
+
+```rust
+use cratesiover::Status;
+
+let query = cratesiover::query("cratesiover", env!CARGO_PKG_VERSION)).unwrap();
+
+match query {
+  Status::Behind => println!("crate is behind the version on crates.io"),
+  Status::Equal => println!("crate is equal to the version on crates.io"),
+  Status::Ahead => println!("crate is ahead of the version on crates.io"),
+}
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
+//! [![Build Status](https://travis-ci.com/kurtlawrence/cratesiover.svg?branch=master)](https://travis-ci.com/kurtlawrence/cratesiover) [![Latest Version](https://img.shields.io/crates/v/cratesiover.svg)](https://crates.io/crates/cratesiover) [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cratesiover) [![codecov](https://codecov.io/gh/kurtlawrence/cratesiover/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cratesiover)
+//! [![Rustc Version 1.30+](https://img.shields.io/badge/rustc-1.30+-blue.svg)](https://blog.rust-lang.org/2018/10/25/Rust-1.30.0.html)
+//!
+//! Query and compare the semver of a crate on crates.io.
+//!
+//! See the [rs docs](https://docs.rs/cratesiover/). [Github repo.](https://github.com/kurtlawrence/cratesiover)
+//!
+//! # Example
+//!
+//! ```rust
+//! use cratesiover::Status;
+//!
+//! let query = cratesiover::query("cratesiover", env!CARGO_PKG_VERSION)).unwrap();
+//!
+//! match query {
+//!   Status::Behind => println!("crate is behind the version on crates.io"),
+//!   Status::Equal => println!("crate is equal to the version on crates.io"),
+//!   Status::Ahead => println!("crate is ahead of the version on crates.io"),
+//! }
+//! ```
+#![warn(missing_docs)]
+
 use reqwest;
 use semver::Version;
+use std::cmp::Ordering;
 
 /// The comparitive status of the version query.
 #[derive(Debug, PartialEq)]
@@ -13,19 +36,31 @@ pub enum Status {
 }
 
 /// Errors in requesting or parsing the query.
+#[derive(Debug)]
 pub enum Error {
 	/// Failed to parse the response for a max version of the crate.
 	ParseError,
+	/// Failed to parse the reponse into a `semver::Version`.
+	SemVerError(semver::SemVerError),
+	/// Failed to successfully make a request to or receive a response from `crates.io`.
+	RequestError(reqwest::Error),
 }
 
 /// Get the `crates.io` version of the specified crate.
 pub fn get(crate_name: &str) -> Result<Version, Error> {
-	Version::parse(parse(web_req(crate_name)?))
+	Version::parse(parse(&web_req(crate_name)?)?).map_err(|e| Error::SemVerError(e))
 }
 
 /// Gets the `crates.io` version of the specified crate and compares it to the specified version.
-pub fn query(crate_name: &str, version: &str) -> Result<Status, ()> {
-	get(crate_name)?
+///
+/// # Example
+/// ```rust
+/// let query = query::("cratesiover", "0.1.0").unwrap();
+/// assert_eq!(query, Status::Behind(Version::parse("1.0.0")));
+/// ```
+pub fn query<V: Into<Version>>(crate_name: &str, version: V) -> Result<Status, Error> {
+	let version: Version = version.into();
+	Ok(cmp(&version, &get(crate_name)?))
 }
 
 fn parse(text: &str) -> Result<&str, Error> {
@@ -36,21 +71,25 @@ fn parse(text: &str) -> Result<&str, Error> {
 	}
 }
 
-fn web_req(crate_name: &str) -> Result<String, ()> {
-	match reqwest::get(&format!("https://crates.io/api/v1/crates/{}", crate_name)) {
-		Ok(mut response) => match response.text() {
-			Ok(text) => Ok(text),
-			Err(_) => Err(()),
-		},
-		Err(_) => Err(()),
+fn web_req(crate_name: &str) -> Result<String, Error> {
+	reqwest::get(&format!("https://crates.io/api/v1/crates/{}", crate_name))
+		.map_err(|e| Error::RequestError(e))?
+		.text()
+		.map_err(|e| Error::RequestError(e))
+}
+
+fn cmp(current: &Version, cratesio: &Version) -> Status {
+	match current.cmp(cratesio) {
+		Ordering::Less => Status::Behind,
+		Ordering::Equal => Status::Equal,
+		Ordering::Greater => Status::Ahead,
 	}
 }
 
 #[test]
 fn parse_test() {
-	assert_eq!(parse(r#""max_version":"0.4.2""#, Some("0.4.2")));
-	assert_eq!(parse(r#""max_version":"0..2""#, Some("0..2")));
-	assert_eq!(parse(r#""max_version0.4.2""#, None));
+	assert_eq!(parse(r#""max_version":"0.4.2""#).unwrap(), "0.4.2");
+	assert_eq!(parse(r#""max_version":"0..2""#).unwrap(), "0..2");
 }
 
 #[test]
